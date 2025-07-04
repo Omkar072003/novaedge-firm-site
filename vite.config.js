df2e1f7325b7cb1e59567a6a5c8fff00fe85aae7
Omkar Nawale <168687409+Omkar072003@users.noreply.github.com>
2025-07-05 00:12:30 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,4 +2,8 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
     base: './',
+    assetsInclude: ['**/*.png', '**/*.jpg'],
+    server: {
+        open: true, // Automatically open browser
+    },
 });
